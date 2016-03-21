1ac8ce0d6c99f3f0431e6c828d0e3994f45f1e80
jay <JayZhou@fih-foxconn.com.cn>
2016-03-21 13:43:27 +0800

    update

--- python/tool_data_cleaning.py
+++ python/tool_data_cleaning.py
@@ -11,10 +11,24 @@ sys.path.insert(0,path)
 import functions as func
 
 
+def delete_history_data(table_name,expire_days):
+    func.mysql_exec('CREATE TABLE '+table_name+'_clean_temp AS SELECT * FROM '+table_name+' WHERE create_time >= from_unixtime(unix_timestamp(now())-('+expire_days+'*24*60*60));','')
+    func.mysql_exec('TRUNCATE TABLE '+table_name,'')
+    func.mysql_exec('INSERT INTO '+table_name+' SELECT * FROM '+table_name+'_clean_temp','')
+    func.mysql_exec('DROP TABLE IF EXISTS '+table_name+'_clean_temp;','')
 def main():
+    delete_table_list = ['mysql_status_history',\
+                         'mysql_bigtable_history',\
+                         'mysql_status_extend',\
+                         'mysql_replication_history',\
+                         'mysql_connected',\
+                         'alarm_history']
+    expire_days='7'
+    print "start to clean the data of history tables"
     try:
-        func.mysql_exec('truncate table alarm_history;','')
-    
+	for table_name in delete_table_list:
+            delete_history_data(table_name,expire_days)
+        print "successfully clean the data of history tables"
     except Exception, e:
         print e
         sys.exit(1)
